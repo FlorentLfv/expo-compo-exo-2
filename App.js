984caeb55f9ed074d18ed44971cc3a17040bc717
Florent <florentlfv@gmail.com>
2022-05-10 13:49:56 +0200

    exercice 2

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImg}>
+        <Image source={require('./assets/rover.jpg')} style={styles.image} />
+      </View>
+      <View style={styles.containerText}>
+        <Text>Le rover Opportunity</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row'
   },
+  containerImg: {
+    flex: 3
+  },
+  image: {
+    width: '100%',
+  },
+  containerText: {
+    flex: 1,
+    justifyContent: 'center'
+  }
 });
